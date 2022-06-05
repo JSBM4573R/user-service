a545f762a5524e42d1557c7c2452b95f00ad6ef8
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-04 22:01:34 -0500

    Se crea la carpeta VO (Value Object) y se crea los archivos Department.java y ResponseTemplateVO.java para el enrutamiento de los objetos.

---
+++ src/main/java/com/microservice/userservice/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.microservice.userservice.VO;
+
+import com.microservice.userservice.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
