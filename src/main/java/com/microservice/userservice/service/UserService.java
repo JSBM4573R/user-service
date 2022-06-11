c9f8460dc64ab0da79df1ad3207c802db25f301c
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-10 23:41:19 -0500

    Se cambia el url del restTemplate para el Department service en el UserService

--- src/main/java/com/microservice/userservice/service/UserService.java
+++ src/main/java/com/microservice/userservice/service/UserService.java
@@ -30,7 +30,7 @@ public class UserService {
         User user = userRepository.findByUserId(userId);
 
         Department department =
-                restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId(),Department.class);
+                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(),Department.class);
         vo.setUser(user);
         vo.setDepartment(department);
 
